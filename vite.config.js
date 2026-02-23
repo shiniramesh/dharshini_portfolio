b1b656cfc10f65d578efdc21a6675469f409405f
shiniramesh <shinirameshh@gmail.com>
2026-02-23 23:49:15 +0000

    Fix image casing for linux build

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // ← ensures assets load correctly on Netlify
+  base: "/", 
+  assetsInclude: ['**/*.JPG'], // ← This fixes the "Invalid JS syntax" error for images
 });
